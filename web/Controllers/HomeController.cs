5d814016db67b1f21078f0b6a3da34e4a36c31cf
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2022-12-27 19:11:40 -0300

    cria camadas e projeto web

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -1,31 +1,30 @@
 ﻿using System.Diagnostics;
+using database.mysql;
 using Microsoft.AspNetCore.Mvc;
+using negocio.models;
+using negocio.services;
 using web.Models;
 
 namespace web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
+    ProdutoServico produtoServico = new ProdutoServico(new RepositorioMYSQL<Produto>());
+    
     public IActionResult Index()
     {
+        var listaDeProdutos = produtoServico.BuscarTodos();
+        int? quantidadeDeProdutos = listaDeProdutos.Count();
+        int quantidade = 0;
+        foreach (Produto produto in listaDeProdutos)
+        {
+            quantidade += produto.Quantidade;
+        }
+        int? quantidadeDeItensNoEstoque = quantidade;
+        ViewBag.quantidadeDeProdutos = quantidadeDeProdutos;
+        ViewBag.quantidadeDeItensNoEstoque = quantidadeDeItensNoEstoque;
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }
---
+++ web/Controllers/ProdutosController.cs
@@ -0,0 +1,50 @@
+
+using database.mysql;
+using Microsoft.AspNetCore.Mvc;
+using negocio.models;
+using negocio.services;
+
+
+namespace web.Controllers;
+
+public class ProdutosController : Controller
+{
+    ProdutoServico produtoServico = new ProdutoServico(new RepositorioMYSQL<Produto>());
+    public IActionResult Index()
+    {
+        ViewBag.produtos = produtoServico.BuscarTodos();
+        return View();
+    }
+    public IActionResult Cadastrar([FromForm] Produto produto)
+    {
+        if( string.IsNullOrEmpty(produto.Nome) )
+        {
+            ViewBag.erro = "O nome não pode ser vazio!";
+            return View();
+        }
+        
+        produtoServico.Salvar(produto);
+        return Redirect("/produtos");
+    }
+    
+    [Route("/produtos/{id}/atualizar")]
+    public IActionResult Atualizar([FromRoute] int id,[FromForm] Produto produto)
+    {
+        produto.Id = id;
+        produtoServico.Salvar(produto);
+        return Redirect("/produtos");
+    }
+    
+     [Route("/produtos/{id}/deletar")]
+    public IActionResult Apagar([FromRoute] int id)
+    {
+        produtoServico.ApagarPorId(id);
+        return Redirect("/produtos");
+    }
+
+    public IActionResult Novo()
+    {
+        return View();
+    }
+
+}
